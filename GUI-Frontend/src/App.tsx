cdf554a332d2a82cae6f91ad7e9f02983a6cd23a
Paulifyer <ethansoh0308@gmail.com>
2025-10-15 11:59:46 +0800

    Add Webscraper functionality to fetch TLE information from SATNOGSDB

--- GUI-Frontend/src/App.tsx
+++ GUI-Frontend/src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 import Header from "./Util/Header";
 import PopupMenu from "./Util/Popup";
 import Globe from "react-globe.gl";
-import { generateOrbit } from "./Util/Predictor"
+import { generateOrbit } from "./Util/Predictor.ts"
 
 /**
  * This type describes the location of a station and its name
@@ -54,21 +54,14 @@ interface FormProp {
   handleLong: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-/**
- * Dummy satellite data gotten from a website. In the future maybe use a JSON to store the TLE data when it is updated so
- * that when the data is updated it will re render the program.
- */
-const satellites = [{
-  name : "SCOOB-II",
-  lastUpdate : new Date(Date.now()),
-  TLE1 : "1 63211U 25052B   25237.20035185  .00010988  00000-0  47621-3 0  9999",
-  TLE2 : "2 63211  97.4291 130.4139 0006211  71.3692 288.8218 15.22533988 24904"
-}, {
-  name : "NUSHSat1",
-  lastUpdate : new Date(Date.now()),
-  TLE1 : "1 57484U 23109D   24006.48621913  .00017746  00000-0  90245-3 0  9997",
-  TLE2 : "2 57484   4.9510 145.7547 0005430 219.0242 140.9473 15.13763583 24325"
-}]
+
+
+interface satellite {
+  name : string,
+  lastUpdate : Date,
+  TLE1 : string,
+  TLE2 : string
+}
 
 const AddForm = ({
   inputLat,
@@ -123,7 +116,7 @@ const App = () => {
   const [clickedPoint, setClickedPoint] = useState("");
   const [elapsedTime, setElapsedTime] = useState(0);
   const [isMax, setIsMax] = useState(false);
-  
+  const [satData, setSatData] = useState<SatStruct[] | null>(null);
   const [lData, setLData] = useState<labels[]>([
     {
       name: "STAR E7",
@@ -167,19 +160,28 @@ const App = () => {
     }, 60000);
     return () => clearInterval(minuteCounter)
   }, [])
-      
-  const satData = useMemo( //Variable that populates/repopulates itself with the coordinates of the orbit of a satellite
-    () => {
-      const data = satellites.map((satellite) => { 
-        return { 
-          name : satellite.name, 
-          lastUpdate : satellite.lastUpdate, 
-          path : generateOrbit(satellite.TLE1, satellite.TLE2) 
+  
+  /**
+   * This fetches data from the webscraper running on the localhost server
+   * and fetches the TLE and last updated time from the site
+   */
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch('http://localhost:4000/api/satellites');
+      if (!res.ok) throw new Error('failed to fetch Satellite Data');
+      const data = (await res.json()) as satellite[];
+      let satellites = data.map((satellite) => {
+        return {
+          name: satellite.name,
+          lastUpdate: satellite.lastUpdate,
+          path: generateOrbit(satellite.TLE1, satellite.TLE2)
         }
       })
-      return data;
-    }, [satellites, isMax]
-  );
+      setSatData(satellites);
+    }
+    getData();
+  }, [isMax])
+  
   
   /**
    * Uses the indexes of the orbit array to track the live location of the satellite based on the TLE generated orbit
@@ -187,16 +189,16 @@ const App = () => {
   */
   const particlesData = useMemo( 
     () => {
-      var data : PartStruct[] = satData.map((sat) => {
+      if (!satData) return null;
+      var data : PartStruct[] = (satData as SatStruct[]).map((sat) => {
         var temp = sat.path[elapsedTime]
-        console.log(temp);
           return {
             name : sat.name,
             point : temp
           }
       })
       return data;
-  }, [satellites, elapsedTime])
+  }, [satData, elapsedTime])
   return (
     <div className="content">
       <Header />
@@ -204,7 +206,7 @@ const App = () => {
         <div id="selector">
           <button
             id="closeButton"
-            onClick={() => setIsAdding([false, isAdding[1], isAdding[1]])}
+            onClick={() => setIsAdding([false, isAdding[1], isAdding[2]])}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -274,6 +276,8 @@ const App = () => {
           name={clickedPoint}
           />
         <Globe
+          width={window.innerWidth}
+          height={window.innerHeight - 26.4}
           backgroundColor="#030a18"
           backgroundImageUrl={nightSky}
           bumpImageUrl={earthTop}
@@ -284,7 +288,7 @@ const App = () => {
           labelText={(d) => (d as labels).name}
           labelSize={1}
           labelDotRadius={0.1}
-          pathsData={satData}
+          pathsData={satData as SatStruct[]}
           pathLabel="name"
           pathPoints="path"
           pathColor={() => ["rgba(27, 255, 46, 1)", "rgba(254, 230, 9, 1)"]}
@@ -293,14 +297,18 @@ const App = () => {
             setClickedPoint((path as SatStruct).name);
             setIsPopped(true);
           }}
+          onPathHover={(path, prevPath) => {
+            console.log(path);
+            console.log(prevPath);
+          }}
           
           pathDashLength={0.01}
           pathDashGap={0.004}
           pathDashAnimateTime={100000}
           pathPointAlt={(pnt) => pnt[2]}
           pathTransitionDuration={4000}
-          particlesData={particlesData}
-          particlesList={particlesData}
+          particlesData={particlesData as PartStruct[]}
+          particlesList={particlesData as PartStruct[]}
           particleLat={(d) => (d as PartStruct).point[0]}
           particleLng={(d) => (d as PartStruct).point[1]}
           particleAltitude={(d) => (d as PartStruct).point[2]}
